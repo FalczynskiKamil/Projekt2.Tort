59ee441184fe11b9d3b9f4c0286630ab00c0daa8
kamil <kamyl.falczynski@gmail.com>
2023-11-08 11:51:03 +0100

    testowy

--- src/main/java/projekt/kalkulator/Kalkulator.java
+++ src/main/java/projekt/kalkulator/Kalkulator.java
@@ -3,11 +3,23 @@ package projekt.kalkulator;
 import java.util.Scanner;
 
 public class Kalkulator {
-    public static int dodaj(int a,int b) {
-        Scanner dod = new Scanner(System.in);
-        a = dod.nextInt();
-        b = dod.nextInt();
-        System.out.println(a+b);
+    //dodanie metody
+public Integer dodaj(String dane){
+    //jeśli metoda jest pusta to zwraca 0
+    if(dane.isEmpty()){
         return 0;
     }
+    //dodanie string ktory pozwala zeby liczby byly rozdzielane przez znaki
+    String[] ignorowacZnaki = dane.split(",|/n");
+    //stworzenie int który przechowuje wynik
+    Integer wynikDodawania = 0;
+    for (int i = 0; i <ignorowacZnaki.length ; i++){
+        String liczba = ignorowacZnaki[i];
+    //zmienienie znakow string na int
+    wynikDodawania += Integer.parseInt(liczba);
+
+    }
+    // zwrocenie wyniku
+    return wynikDodawania;
 }
+    }
